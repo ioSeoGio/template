20fbc3a154e03bfa816ff6e9145ddb7b9d18e1db
seog <ioSeoGio@gmail.com>
2021-09-08 17:30:30 +0300

    Angular (stable)

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,6 +23,8 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/app.js',
+        
     ];
     public $depends = [
         'yii\web\YiiAsset',
